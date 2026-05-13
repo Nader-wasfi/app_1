56d42788eb0400750477ae5e2c006283bde86501
Nader Wasfi <naderwasfi94@gmail.com>
2026-05-13 21:20:10 +0300

    new app

--- lib/main.dart
+++ lib/main.dart
@@ -7,15 +7,14 @@ void main() {
 class MyApp extends StatelessWidget {
   const MyApp({super.key});
 
-  // This widget is the root of your application.
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
       debugShowCheckedModeBanner: false,
       title: 'First App',
-      
+      home: Scaffold(
+        body: const Center(child: Text('Welcome to your App')),
+      ),
     );
   }
 }
-
-
